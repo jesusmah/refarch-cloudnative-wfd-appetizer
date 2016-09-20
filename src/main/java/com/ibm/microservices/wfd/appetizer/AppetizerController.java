6c10d88cec5a5be4f9bf35ca6b2c44ab9e94e462
Jesus Almaraz <almarazj@ie.ibm.com>
2016-09-20 18:43:50 +0100

    Swagger API implemented and build script modified accordignly

--- src/main/java/com/ibm/microservices/wfd/appetizer/AppetizerController.java
+++ src/main/java/com/ibm/microservices/wfd/appetizer/AppetizerController.java
@@ -2,14 +2,17 @@ package com.ibm.microservices.wfd.appetizer;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import org.springframework.stereotype.Component;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 import com.ibm.microservices.wfd.appetizer.model.Appetizer;
 
@@ -21,7 +24,14 @@ public class AppetizerController {
     @Autowired
     private AppetizerConfiguration config;
 
-    @RequestMapping("/menu")
+    @ApiOperation(value = "Get the appetizers", notes = "Returns a list of the appetizers available on the menu")
+    @RequestMapping(method = RequestMethod.GET, path="/appetizers", produces = "application/json")
+    @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "Success", response = Appetizer.class),
+      @ApiResponse(code = 401, message = "Unauthorized"),
+      @ApiResponse(code = 403, message = "Forbidden"),
+      @ApiResponse(code = 404, message = "Not Found"),
+      @ApiResponse(code = 500, message = "Failure")})
     public Appetizer getMealMenu() {
       Appetizer local = new Appetizer();
       local.setMenu(this.config.getMenu());
